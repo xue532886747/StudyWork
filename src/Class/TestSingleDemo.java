7bd42e27590eeeb51d5334834168ae0446758f54
sherlock <532886747@qq.com>
2018-08-23 18:04:30 +0800

    其中Person类改为了Newman类

--- src/Class/TestSingleDemo.java
+++ src/Class/TestSingleDemo.java
@@ -2,18 +2,18 @@ package Class;
 
 public class TestSingleDemo {
 	public static void main(String[] args) {
-		Person p;
-		p = Person.getPerson();
+		Newman p;
+		p = Newman.getPerson();
 		System.out.println("name:"+p.name);
 	}
 }
-class Person{
+class Newman{
 	String name;
-	private static final Person PERSON = new Person();
-	private Person() {
+	private static final Newman PERSON = new Newman();
+	private Newman() {
 		name = "kehr";
 	}
-	public static Person getPerson() {
+	public static Newman getPerson() {
 		return PERSON;
 	}
 }
